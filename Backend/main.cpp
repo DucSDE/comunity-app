ac5816de02b35b14e49f91ac29e5d5fd248c375d
Duc-Hoang <hoanghuuduc777@gmail.com>
2022-08-13 07:07:02 +0700

    initial project

---
+++ Backend/main.cpp
@@ -0,0 +1,26 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickWindow>
+#include <QQmlContext>
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+    QQuickWindow::setSceneGraphBackend("software");
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    QQmlContext *rootContext = engine.rootContext();
+    const QUrl url(QStringLiteral("qrc:/UI/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
---
+++ UI/main.qml
@@ -0,0 +1,9 @@
+import QtQuick 2.12
+import QtQuick.Window 2.12
+
+Window {
+    width: 640
+    height: 480
+    visible: true
+    title: "Comunity Application Desktop 1.0.0"
+}
